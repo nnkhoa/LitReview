984ec42b9653b8888c40c52f23bf1f7848ae4e8c
nnkhoa <n.nhukhoa2209@gmail.com>
2020-08-27 17:25:42 +0200

    test methods for visualizing word graph

--- Godbole2007/graph_networkx.py
+++ Godbole2007/graph_networkx.py
@@ -1,18 +1,21 @@
-import networkx
-
 import lexicon_builder
-
-import matplotlib.pyplot as plt
+import networkx
 
 if __name__ == '__main__':
     seeded_word_list = [('good', 1), ('bad', -1)]
-
+    # seeded_word_list = [('good', 1)]
     connections = lexicon_builder.build_connections(0, seeded_word_list, [], seeded_word_list)
 
     g = networkx.Graph()
 
     g.add_edges_from(connections)
 
-    networkx.draw(g, with_labels=True)
+    print(g.number_of_nodes())
 
-    plt.show()
+    g = lexicon_builder.flip_filter(g, seeded_word_list, 3)
+
+    print(g.number_of_nodes())
+
+    lexicon_builder.draw_word_graph(g)
+
+    
