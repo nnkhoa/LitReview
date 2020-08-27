c5568434594c5cd4552c8c5de9e7e62cde3567c5
nnkhoa <n.nhukhoa2209@gmail.com>
2020-08-27 17:27:27 +0200

    moved draw graph method to this file, added functions to calculate significance score and sentiment flips in path, uses networkx library instead of writing my own

--- Godbole2007/lexicon_builder.py
+++ Godbole2007/lexicon_builder.py
@@ -1,7 +1,11 @@
 import itertools
 
+import matplotlib.pyplot as plt
+
+import networkx
+from networkx.drawing.nx_agraph import write_dot, graphviz_layout
+
 from nltk.corpus import wordnet as wn
-from WordGraph import Node, Graph
 
 def get_sense_key(wn_key):
     sense_attributes = wn_key.split('%')
@@ -87,3 +91,68 @@ def check_duplicate_seed(word, seeded_word_list):
             return True
     
     return False
+
+
+def draw_word_graph(word_graph):
+    names = [node[0] for node in word_graph.nodes()]
+    
+    labels = {word_graph.nodes()[i] : names[i]  for i in range(len(names))}
+
+    positive_node = [node for node in word_graph.nodes() if node[1] == 1]
+    negative_node = [node for node in word_graph.nodes() if node not in positive_node]
+
+    pos = networkx.spring_layout(word_graph)
+    
+    networkx.draw_networkx_nodes(word_graph, pos, nodelist=positive_node, 
+                                                node_color='r', 
+                                                node_size=600, 
+                                                alpha=1)
+    networkx.draw_networkx_nodes(word_graph, pos, nodelist=negative_node, 
+                                                node_color='g', 
+                                                node_size=600, 
+                                                alpha=1)
+
+    networkx.draw_networkx_edges(word_graph, pos, width=1.0, alpha=1)
+    networkx.draw_networkx_labels(word_graph, pos, labels=labels, font_size=6)
+
+    plt.axis('off')
+    plt.show()
+
+
+def count_flip(path):
+    count = 0
+    for i, j in zip(range(0, len(path) - 1), range(1, len(path))):
+        if path[i][1]*path[j][1] == -1:
+            count+=1
+    return count
+
+
+def flip_filter(word_graph, seeded_word_list, threshold):
+    for node in word_graph.nodes():
+        if node in seeded_word_list:
+            continue
+        for seeded_word in seeded_word_list:
+            for path in networkx.all_simple_paths(word_graph, seeded_word, node):
+                count = count_flip(path)
+        if count > threshold:
+            word_graph.remove_node(node)
+            
+    return word_graph        
+
+def get_significance_score(word_graph, seeded_word_list, constant=1.1):
+    if constant <= 1:
+        print('constant will be set to 1.1')
+        constant = 1.1
+    
+    sig_score = []
+
+    for node in word_graph.nodes():
+        if node in seeded_word_list:
+            continue
+        for seeded_word in seeded_word_list:
+            for path in networkx.all_simple_paths(word_graph, seeded_word, node):
+                score+=1/pow(constant, len(path))
+        
+        sig_score.append({node:score})
+    
+    return sig_score
